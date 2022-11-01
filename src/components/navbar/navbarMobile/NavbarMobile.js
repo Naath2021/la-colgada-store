aaa835234eb78aacd8d30727473e5c33e1801e9f
Nathaly Learmonth <learmonth.nathaly@gmail.com>
2022-10-31 22:17:11 -0300

    queries on cartWidget, navbarMobile, footer, home, orderCheckout, faq, cartItems

--- src/components/navbar/navbarMobile/NavbarMobile.js
+++ src/components/navbar/navbarMobile/NavbarMobile.js
@@ -13,7 +13,7 @@ function NavbarMobile() {
             <Link className='link-router mobile-icons' to={"/"}><Popup content='home' trigger={<FontAwesomeIcon icon={faHouse} />} basic /></Link>
             <Link className='link-router mobile-icons' to={"products"}><Popup content='productos' trigger={<FontAwesomeIcon icon={faHandSparkles} />} basic /></Link>
             <Link className='link-router mobile-icons' to={"preguntas-frecuentes"}><Popup content='preguntas frecuentes' trigger={<FontAwesomeIcon icon={faCircleQuestion} />} basic /></Link>
-            <Cart />
+            <Cart /> 
         </div>
     );
 }
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -46,19 +46,19 @@ const Home = () => {
                 <div className="brand-details">
                     <div className="detail-container" data-aos="zoom-in" data-aos-duration="1000" data-aos-delay="50">
                         <FontAwesomeIcon icon={faGlobe} className="icon" />
-                        <h2 className="texts detail-title">envíos a todo el país</h2>
+                        <h2 className="titles-web detail-title">envíos a todo el país</h2>
                         <h3 className="texts detail-desc">compra desde cualquier parte de Argentina. Consulta para envíos internacionales</h3>
                     </div>
 
                     <div className="detail-container" data-aos="zoom-in" data-aos-duration="1200" data-aos-delay="250">
                         <FontAwesomeIcon icon={faHandSparkles} className="icon" />
-                        <h2 className="texts detail-title">piezas únicas</h2>
+                        <h2 className="titles-web detail-title">piezas únicas</h2>
                         <h3 className="texts detail-desc">nuestras piezas son moldeadas y pintadas a mano, nada de industrias, ¡sólo creatividad!</h3>
                     </div>
 
                     <div className="detail-container" data-aos="zoom-in" data-aos-duration="1400" data-aos-delay="400">
                         <FontAwesomeIcon icon={faTruck} className="icon" />
-                        <h2 className="texts detail-title">devoluciones seguras</h2>
+                        <h2 className="titles-web detail-title">devoluciones seguras</h2>
                         <h3 className="texts detail-desc">15 días para cambiar tu producto de manera gratuita</h3>
                     </div>
                 </div>
